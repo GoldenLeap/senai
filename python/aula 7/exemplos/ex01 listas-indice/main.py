35a8bb91e13a3d956c04fc9dff0359e14134cf16
Gabriel Bernardi <115042820+GoldenLeap@users.noreply.github.com>
2025-05-26 13:41:34 -0300

    Adiciona primeiro exemplo de listas em python

---
+++ python/aula 7/exemplos/ex01 listas-indice/main.py
@@ -0,0 +1,12 @@
+bancos = ["Banco do Brasil", "Caixa", "Santander"]
+print(bancos)
+# Resultado: ['Banco do Brasil', 'Caixa', 'Santander']
+
+
+bancos[1] = "Itaú"
+print(bancos)
+# Resultado: ['Banco do Brasil', 'Itaú', 'Santander']
+
+bancos[-1] = "CS6"
+print(bancos)
+# Resultado: ['Banco do Brasil', 'Itaú', 'CS6']
