bd90157e2d2487954fd49a9661afe711840d739c
Gabriel Bernardi <115042820+GoldenLeap@users.noreply.github.com>
2025-05-26 13:54:58 -0300

    Adiciona exemplo de incrementação

--- python/aula 7/exemplos/ex01 listas-indice/main.py
+++ python/aula 7/exemplos/ex01 listas-indice/main.py
@@ -10,3 +10,10 @@ print(bancos)
 bancos[-1] = "CS6"
 print(bancos)
 # Resultado: ['Banco do Brasil', 'Itaú', 'CS6']
+
+bancos = bancos + ["Bradesco", "Nubank"]
+print(bancos)
+# Resultado: ['Banco do Brasil', 'Itaú', 'CS6', 'Bradesco', 'Nubank']
+bancos += ["Safra", "Caixa", "Santander"]
+print(bancos)
+# Resultado ['Banco do Brasil', 'Itaú', 'CS6', 'Bradesco', 'Nubank', 'Safra', 'Caixa', 'Santander']
