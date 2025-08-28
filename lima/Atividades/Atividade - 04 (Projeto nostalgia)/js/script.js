c1d8c054be69766aeea8ee6bef659ecf64b8d43f
Gabriel Bernardi <115042820+GoldenLeap@users.noreply.github.com>
2025-08-28 16:51:46 -0300

    Atividade 4 - projeto nostalgia

---
+++ lima/Atividades/Atividade - 04 (Projeto nostalgia)/js/script.js
@@ -0,0 +1,24 @@
+const imgs = {
+    "cdz" : document.getElementsByClassName("img-cdz"),
+    "pc" : document.getElementsByClassName("img-pc"),
+    "db" : document.getElementsByClassName("img-db"),
+    "sm" : document.getElementsByClassName("img-sm"),
+    "jb" : document.getElementsByClassName("img-jb"),
+    "pk" : document.getElementsByClassName("img-pk")
+}
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+    const img = card.querySelector("img");
+    const staticSrc = img.getAttribute("data-static");
+    const gifSrc = img.getAttribute("data-gif");
+
+    card.addEventListener("mouseover", () => {
+        img.src = gifSrc;
+    });
+
+    card.addEventListener("mouseout", () => {
+        img.src = staticSrc;
+    });
+});
