559dbc229a0e4489034371fbb46f73b9bd12ad8d
Gabriel Bernardi <115042820+GoldenLeap@users.noreply.github.com>
2025-10-30 12:25:52 -0300

    Atualiza lógica de verificação de usuário e ajusta exibição de informações no perfil

--- lima/Projetos/TechFit/app/controllers/profileController.php
+++ lima/Projetos/TechFit/app/controllers/profileController.php
@@ -32,17 +32,41 @@ function verificarUsuario()
 
     $db = Connect::conectar();
     //$stmt = $db->prepare("SELECT tipo, pfp FROM usuario WHERE id_usuario = :id_usuario");
-    $stmt = $db->prepare("SELECT nome_aluno FROM Alunos WHERE id_usuario = :id_usuario");
+    $stmt = $db->prepare("SELECT id_usuario email, tipo, avatar FROM Usuarios WHERE id_usuario = :id_usuario");
     $stmt->bindParam(":id_usuario", $_SESSION['user_id']);
     $stmt->execute();
     $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+    if (!$usuario){
+        header('location: /');
+        exit;
+    }
+    $userTipo = $usuario['tipo'];
+    $userNome = 'User';
 
-    return [
-        'user_name' => $usuario['nome_aluno'] ?? 'Cleitin',
-        'user_tipo' => $usuario['tipo'] ?? 'Aluno',
-
+    switch ($userTipo) {
+        case 'aluno':
+            $stmt = $db->prepare("SELECT nome_aluno AS nome FROM Alunos WHERE id_usuario = :id_usuario");
+            break;
+        case 'funcionario':
+            $stmt = $db->prepare('SELECT nome_funcionario  AS nome FROM Funcionarios WHERE id_usuario = :id_usuario');
+            break;
+        default:
+            $stmt = null;
+            break;
+        }
+    if($stmt){
+        $stmt->bindParam(':id_usuario', $_SESSION['user_id']);
+        $stmt->execute();
+        $dadosUsuario = $stmt->fetch(PDO::FETCH_ASSOC);
+        $userNome = $dadosUsuario['nome'] ?? $userNome;
+    }
+    return[
+        'user_name' => $userNome,
+        'user_tipo' => ucfirst($userTipo),
+        'user_avatar' => $usuario['avatar'] ?? __DIR__ . '/../../public/assets/images/avatar.png '
     ];
 }
+
 
 
 function carregarConteudoPagina($page, $tipo = null)
